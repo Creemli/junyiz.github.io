ae9afe2092207dbdfca06da7487f581370a614c9
Junyiz <ipaper@live.cn>
2015-06-20 16:24:55 +0800

    使用Backbone.js的风格重构

--- digg/diggs.js
+++ digg/diggs.js
@@ -3,44 +3,54 @@ var TagsObject = AV.Object.extend('tags');
 var tagsQuery = new AV.Query(TagsObject);
 var DiggsObject = AV.Object.extend('diggs');
 var diggsQuery = new AV.Query(DiggsObject);
-var tagsList = document.querySelector('.tags-list');
-var diggsList = document.querySelector('.diggs-list');
-
-tagsQuery.find({
-    success: function(results) {
-        var h = '';
-        results.forEach(function(r) {
-            h += '<a href="' + r.attributes.url + '" data-tag="' + r.attributes.tag + '">' + r.attributes.txt + '</a>'
-        });
-        tagsList.innerHTML = h;
+var AppState = AV.Object.extend("AppState", {
+    defaults: { tag: "" }
+});
+var AppView = AV.View.extend({
+    el: '.container',
+    initialize: function () {
+        this.getTags();
     },
-    error: function(error) {}
-});
-
-tagsList.addEventListener('click', function(e) {
-    e.preventDefault();
-    var t = e.target;
-    var tag = t.getAttribute('data-tag');
-
-    diggsQuery.contains('tag', tag).find({
-        success: function(results) {
-            var h = '';
-            results.forEach(function(r) {
-                h += '<li><p>' + r.attributes.info + '</p><p><img src="' + r.attributes.thumb + '"/><p class="intro">' + r.attributes.intro + '</p></li>'
-            });
-            diggsList.innerHTML = h;
-        },
-        error: function(error) {}
-    });
-});
-
-diggsQuery.find({
-    success: function(results) {
-        var h = '';
-        results.forEach(function(r) {
-            h += '<li><p>' + r.attributes.info + '</p><p><img src="' + r.attributes.thumb + '"/><p class="intro">' + r.attributes.intro + '</p></li>'
+    tagsTpl: _.template($('#tags-tpl').text()),
+    diggsTpl: _.template($('#diggs-tpl').text()),
+    getTags: function () {
+        tagsQuery.find({
+            success: this.renderTags.bind(this),
+            error: function(error) {}
         });
-        diggsList.innerHTML = h;
     },
-    error: function(error) {}
+    renderTags: function (results) {
+        this.$('.tags .list').html(this.tagsTpl({results: results}));
+    },
+    getDiggs: function (tag) {
+        (tag != '' ? diggsQuery.contains('tag', tag) : diggsQuery).find({
+            success: this.renderDiggs.bind(this),
+            error: function(error) {}
+        });
+    },
+    renderDiggs: function (results) {
+        this.$('.diggs .list').html(this.diggsTpl({results: results}));
+    }
 });
+var AppRouter = AV.Router.extend({
+    routes: {
+        "": "all",
+        ":tag": "tag",
+    },
+    initialize: function() {
+        this.view = new AppView;
+        this.state = new AppState;
+        this.state.on('change', function() {
+            var tag = this.state.get('tag');
+            this.view.getDiggs(tag);
+        }, this);
+    },
+    all: function() {
+        this.state.set({ tag: "" });
+    },
+    tag: function(tag) {
+        this.state.set({ tag: tag });
+    }
+});
+new AppRouter;
+AV.history.start();
